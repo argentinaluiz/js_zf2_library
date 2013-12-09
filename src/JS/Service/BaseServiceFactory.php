3295bd80056e2a974537320f0f29242c85860ae0
Luiz Carlos <argentinaluiz@gmail.com>
2013-12-09 00:11:49 -0200

    novo jslibrary

--- src/JS/Service/BaseServiceFactory.php
+++ src/JS/Service/BaseServiceFactory.php
@@ -4,33 +4,14 @@ namespace JS\Service;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
-use JS\Translator\Translator;
 
 class BaseServiceFactory implements FactoryInterface {
 
-    protected $textDomain = 'js';
-    protected $entityName = '';
-
     public function createService(ServiceLocatorInterface $serviceLocator) {
         $entityManager = $serviceLocator->get('doctrine.entitymanager.orm_default');
-        $service = new BaseService($entityManager, $this->getTranslator($serviceLocator), $this->getEntityName());
+        $service = new BaseService($entityManager, $serviceLocator->get('jstranslator'), 'Entity');
         $service->setServiceLocator($serviceLocator);
         return $service;
-    }
-
-    /**
-     * @return \JS\Translator\Translator
-     */
-    public function getTranslator($serviceLocator) {
-        $config = $serviceLocator->get('Configuration');
-        $trConfig = isset($config['translator']) ? $config['translator'] : array();
-        $translator = Translator::factory($trConfig);
-        $translator->setTextDomain($this->textDomain);
-        return $translator;
-    }
-
-    public function getEntityName() {
-        return $this->entityName;
     }
 
 }
