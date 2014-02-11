3b9948bbfeb1f083a9c4a5f5f6d78fe03d18ab6b
Luiz Carlos <argentinaluiz@gmail.com>
2014-02-11 17:19:55 -0200

    fix sql transactions

--- src/JS/Test/JSTestCase.php
+++ src/JS/Test/JSTestCase.php
@@ -10,8 +10,8 @@ class JSTestCase extends \PHPUnit_Framework_TestCase {
     protected $application;
 
     public function init() {
-        $this->application = \tests\Bootstrap::getBootstrap();
         $this->createDataBase();
+        $this->application = \tests\Bootstrap::getBootstrap();
         $this->createTables();
     }
 
@@ -23,12 +23,28 @@ class JSTestCase extends \PHPUnit_Framework_TestCase {
         $password = $params['password'];
         $dbname = $params['dbname'];
         $dbh = new \PDO("mysql:host=$host", $user, $password);
-        if (!$dbh->exec("CREATE DATABASE IF NOT EXISTS `$dbname`;CHARACTER SET utf8 COLLATE utf8_general_ci;")) {
+        if (!$dbh->exec("CREATE DATABASE IF NOT EXISTS `$dbname` CHARACTER SET utf8 COLLATE utf8_general_ci;")) {
             throw new \Exception(print_r($dbh->errorInfo(), true));
         }
+        $dbh = null;
+    }
+
+    public function dropDatabase() {
+        $config = include getcwd() . '/config/autoload/doctrine.test.php';
+        $params = $config['doctrine']['connection']['orm_default']['params'];
+        $host = $params['host'];
+        $user = $params['user'];
+        $password = $params['password'];
+        $dbname = $params['dbname'];
+        $dbh = new \PDO("mysql:host=$host", $user, $password);
+        if (!$dbh->exec("DROP DATABASE IF EXISTS `$dbname`;")) {
+            throw new \Exception(print_r($dbh->errorInfo(), true));
+        }
+        $dbh = null;
     }
 
     public function createTables() {
+        $this->getEntityManager()->clear();
         $em = $this->getEntityManager();
 
         $schemaTool = new \Doctrine\ORM\Tools\SchemaTool($em);
@@ -41,11 +57,12 @@ class JSTestCase extends \PHPUnit_Framework_TestCase {
     }
 
     protected function setUp() {
-        parent::setUp();
         $this->init();
+        parent::setUp();
     }
 
     protected function tearDown() {
+        $this->dropDatabase();
         parent::tearDown();
     }
 
@@ -57,6 +74,9 @@ class JSTestCase extends \PHPUnit_Framework_TestCase {
         return $this->application->getServiceManager();
     }
 
+    /**
+     * @return \Doctrine\ORM\EntityManager
+     */
     public function getEntityManager() {
         return $this->getServiceManager()->get('Doctrine\ORM\EntityManager');
     }
